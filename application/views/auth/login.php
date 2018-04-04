555ccbdcb6db268ca8fb264346782137ee98e051
rodrigog84 <rodrigog.84@gmail.com>
2018-04-03 23:24:26 -0300

    modifica titulo sistema

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -5,7 +5,7 @@
 <!--<![endif]-->
 <head>
   <meta charset="utf-8" />
-  <title>Sistema de Remuneraciones Dynamics | Página Acceso</title>
+  <title>Sistema de Factura Electr&oacute;nica Infosys | Página Acceso</title>
   <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" name="viewport" />
   <meta content="" name="description" />
   <meta content="" name="author" />
@@ -46,7 +46,7 @@
     <div class="login-header">
       <div class="brand">
         <span class="logo"></span>Factura Electr&oacute;nica
-        <small>Acceso a sistemas de Factura Electr&oacute;nica</small>
+        <small>Acceso a sistemas de Factura Electr&oacute;nica Infosys</small>
       </div>
       <div class="icon">
         <i class="fa fa-sign-in"></i>
